644ec328c086d70995c8a579148dce3ee5f4f612
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-02-18 18:07:50 +0500

    minner changes

--- src/components/Desktop4/Home/UnspokenChallengesOfMiddleAgeLife.jsx
+++ src/components/Desktop4/Home/UnspokenChallengesOfMiddleAgeLife.jsx
@@ -46,7 +46,7 @@ function UnspokenChallengesOfMiddleAgeLife() {
         <div className="flex items-center justify-center">
           <a href="https://innovacare.tech/listenerhub/signup">
             <button className="mt-6 bg-[#5200FF] flex flex-row text-white text-[16px] font-[500] py-5 px-10 rounded-full hover:bg-purple-800">
-            You Deserve to Feel Heard. Connect Today
+            You Deserve To Feel Heard. Connect Today
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="21"
