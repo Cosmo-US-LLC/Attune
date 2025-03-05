31ee2469b92abbdeaeac613cc64f267722a44b35
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-03-05 16:43:16 +0500

    listener-recruitment

---
+++ src/components/ListenerRecruitment/Desktop/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Hero from './Hero'
+import HowItWorks from './HowItWorks'
+import SafeSpace from './SafeSpace'
+import YouGrowth from './YouGrowth'
+import ReadyToMake from './ReadyToMake'
+import DiffersTherapyCounseling from './DiffersTherapyCounseling'
+import WhyBecomeListener from './WhyBecomeListener'
+import PrivatePractice from './PrivatePractice'
+
+function ListenerRecruitmentDesktop() {
+  return (
+    <>
+    <Hero />
+    <WhyBecomeListener/>
+    <SafeSpace/>
+    <PrivatePractice/>
+    <HowItWorks/>
+    <DiffersTherapyCounseling/>
+    <ReadyToMake/>
+    <YouGrowth/>
+    </>
+  )
+}
+
+export default ListenerRecruitmentDesktop
